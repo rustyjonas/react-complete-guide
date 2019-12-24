679b8efd10a49382e5cbe6652262e839f7256b15
Rusty Jonas Gallardo <rusty.gallardo@avannubo.com>
2019-12-24 10:54:43 +0100

    Adding Two Way Binding

--- src/App.js
+++ src/App.js
@@ -19,8 +19,18 @@ class App extends Component {
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27},
       ]
-    })
-  }
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26},
+      ]
+    });
+  };
 
   render(){
     return (
@@ -34,7 +44,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!!')}> My Hobbies: Racing
+          click={this.switchNameHandler.bind(this, 'Max!!')}
+          changed={this.nameChangedHandler}> My Hobbies: Racing
           </Person>
         <Person 
           name={this.state.persons[2].name} 
