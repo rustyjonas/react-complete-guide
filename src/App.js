3212f9650e7183bb81a4e9dd71255c82d0f99e7b
Rusty Jonas Gallardo <rusty.gallardo@avannubo.com>
2019-12-24 11:51:51 +0100

    Handling Dynamic Content "The JavaScript Way"

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
-  render(){
+  render () {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
@@ -47,15 +47,10 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { 
-          this.state.showPersons === true ? 
+    let persons = null;
+
+    if (this.state.showPersons) {
+        persons = (
           <div>
             <Person 
             name={this.state.persons[0].name} 
@@ -68,9 +63,20 @@ class App extends Component {
             </Person>
           <Person 
             name={this.state.persons[2].name} 
-            age={this.state.persons[2].name}/>
-          </div> : null
-        }
+            age={this.state.persons[2].age}/>
+          </div>
+      
+        );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it work now'));
